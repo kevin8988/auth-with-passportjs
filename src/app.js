3623e98af2883243ce559e18d7f8fc75f5acf703
KevinChips <kevsilva07@gmail.com>
2019-08-26 15:51:27 -0300

    some routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const authRouter = require("./routes/authRouter");
+const homeRouter = require("./routes/homeRouter.js");
 
 const app = express();
 
@@ -8,8 +10,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 //simple route
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.use(express.json());
+app.use(authRouter);
+app.use(homeRouter);
 
 module.exports = app;

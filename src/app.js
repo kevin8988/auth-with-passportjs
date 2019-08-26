18be8b375050f7c7b454eccd517b40597e32b272
KevinChips <kevsilva07@gmail.com>
2019-08-26 16:12:43 -0300

    login view and more routes

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 //simple route
 app.use(express.json());
-app.use(authRouter);
+app.use("/auth", authRouter);
 app.use(homeRouter);
 
 module.exports = app;
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-router.route("/auth/:provider").get((req, res) => {});
+router.route("/login").get((req, res) => {
+  res.render("login");
+});
+
+router.route("/google").get((req, res) => {
+  res.send("logging google");
+});
+
+router.route("/logout").get((req, res) => {
+  res.send("logg out");
+});
 
 module.exports = router;

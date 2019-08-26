f146741c9717ad065fd9c3727136511ed283ec4a
KevinChips <kevsilva07@gmail.com>
2019-08-26 15:41:09 -0300

    setting up templates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+
+//set up view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//simple route
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 module.exports = app;
